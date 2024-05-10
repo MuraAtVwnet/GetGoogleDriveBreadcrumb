c5d96655626da078afe7187e2c6b5c4a0b70943f
VWNET\mura <mura@vwnet.jp>
2024-05-10 14:44:27 +0900

    1st up

---
+++ GetGoogleDriveBreadcrumb.psm1
@@ -0,0 +1,11 @@
+﻿function GetGoogleDriveBreadcrumb(){
+	$FileFullPath = Get-Clipboard
+
+	$SelectPath = $FileFullPath -replace "^.+\\13_三井製糖" , "google drive > 13_三井製糖"
+	$Pankuzu = $SelectPath -replace "\\", " > "
+	$Pankuzu = $Pankuzu.replace( '"', '')
+
+	Set-Clipboard $Pankuzu
+	Write-Output "done"
+}
+
